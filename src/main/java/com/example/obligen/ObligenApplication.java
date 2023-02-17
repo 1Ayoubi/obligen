9b826203d0a6d704e5c5c3609bdb05da908749a9
1Ayoubi <s362118@oslomet.no>
2023-02-17 15:22:34 +0100

    Initial commit

---
+++ src/main/java/com/example/obligen/ObligenApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObligenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obligen/ObligenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
